c94edc39c589c4833bd687b57cacf08819a46a2f
Tatr1 <ttttt_68@mail.ru>
2022-02-23 17:20:03 +0300

    jar

--- src/test/resources/requests.sql
+++ src/test/resources/requests.sql
@@ -4,3 +4,6 @@ show tables;
 select * from credit_request_entity;
 select * from payment_entity;
 select * from order_entity;
+delete from credit_request_entity where true;
+delete from payment_entity where true;
+delete from order_entity where true;
